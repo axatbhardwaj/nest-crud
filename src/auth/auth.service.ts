a2124d0996683c187227428bbe9f05aa60b5c218
axatbhardwaj <axatbhardwaj@outlook.com>
2023-06-13 17:18:04 +0530

    added a custom error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { user, bookmark } from '@prisma/client';
-@Injectable({})
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+@Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'signup sucessful' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the DB
+    try {
+      // fetch user details from db with same email
+      const result = await this.prisma.user.findMany({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      if (result.length > 0) {
+        console.log(result);
+        throw new ConflictException('Credentials taken');
+      }
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+
+      // SELECT Count(*) FROM user WHERE email = 'email';
+      //return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+    }
   }
 
   signin() {
-    return { msg: 'signin sucessful' };
+    return { msg: 'signin successful' };
   }
 }
